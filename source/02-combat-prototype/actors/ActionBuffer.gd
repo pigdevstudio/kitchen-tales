c04e0c3879da3a1d56b393177bddd7a9c11be3db
Henrique Campos <henriquecampos@pigdev.studio>
2019-06-28 10:22:06 -0300

    ActionBuffer now filters actions based on a provided list of actions

--- source/02-combat-prototype/actors/ActionBuffer.gd
+++ source/02-combat-prototype/actors/ActionBuffer.gd
@@ -4,15 +4,20 @@ extends Node
 Stores a chain of actions with a timespam
 """
 
-signal actions_chain_cleared(action_chain)
 signal actions_chain_changed(action_chain)
+signal actions_chain_cleared
  
 onready var solve_timer = $Timer
 export (float) var clear_time = 0.5
+export (PoolStringArray) var valid_input_actions
 
 var _unsolved_action_chain = []
 
 func _unhandled_input(event):
+	stack_input_event(event)
+
+
+func stack_input_event(event):
 	if not event.is_action_type():
 		return
 	if event.is_echo():
@@ -27,18 +32,17 @@ func _unhandled_input(event):
 		solve_timer.start(clear_time)
 
 
-
 func get_event_action(event):
 	var action_name = ""
 	for action in InputMap.get_actions():
 		if InputMap.action_has_event(action, event):
-			if "ui" in action:
+			if action in valid_input_actions:
+				action_name = action
+			else:
 				continue
-			action_name = action
-	
 	return action_name
 
 
 func clear_unsolved_actions():
-	emit_signal("actions_chain_cleared", _unsolved_action_chain)
 	_unsolved_action_chain.clear()
+	emit_signal("actions_chain_cleared")
