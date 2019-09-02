156955b7b5a86c5c6c7255bd0f47eb62927fc3d5
Henrique Campos <henriquecampos@pigdev.studio>
2019-09-02 09:23:43 -0300

    Fix #55

--- source/actors/physics/Fall.gd
+++ source/actors/physics/Fall.gd
@@ -1,24 +1,26 @@
 extends "res://actors/physics/PlatformCommand.gd"
 
 export (float) var FALL_THRESHOLD = 100.0
+var fell = false
 
 func _physics_process(delta):
 	check_for_fall()
 
 
 func execute():
+	fell = true
 	emit_signal("started")
 
 
 func cancel():
-	set_physics_process(false)
+	fell = false
 	emit_signal("finished")
 
 
 func check_for_fall():
 	if not enabled:
 		return
-	if actor.is_on_floor():
+	if actor.is_on_floor() and fell:
 		cancel()
 	elif actor.velocity.y > FALL_THRESHOLD:
 		execute()
