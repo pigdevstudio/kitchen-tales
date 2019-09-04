6acc16ba35b250fbc2a18deacc0bb0a6ef49363a
Henrique Campos <henriquecampos@pigdev.studio>
2019-09-04 13:41:06 -0300

    Fix #53

--- source/actors/physics/Stop.gd
+++ source/actors/physics/Stop.gd
@@ -3,7 +3,6 @@ extends "res://actors/physics/PlatformCommand.gd"
 func execute():
 	actor.velocity.x = 0.0
 	emit_signal("started")
-#	print("%s Stop executed" % get_parent().name) 
 
 
 func cancel():
--- source/actors/player/buccerino/CutoutCharacter.gd
+++ source/actors/player/buccerino/CutoutCharacter.gd
@@ -4,7 +4,7 @@ var look_direction = 1 setget set_look_direction
 
 func set_look_direction(value):
 	look_direction = value
-	scale.x = look_direction
+	scale.x = abs(scale.x) * look_direction
 
 
 func play(animation):
