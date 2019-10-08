7b8cc9d9e62a8b6f6e03059bd9f509b91c6ad778
Henrique Campos <henriquecampos@pigdev.studio>
2019-10-08 18:06:22 -0300

    Added Carrot character with show/hide animations

---
+++ source/actors/enemies/carrot/CutoutCharacter.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var look_direction = 1 setget set_look_direction
+
+func set_look_direction(value):
+	look_direction = value
+	scale.x = abs(scale.x) * look_direction
+
+
+func play(animation):
+	if $AnimationPlayer.has_animation(animation):
+		$AnimationPlayer.play(animation)
