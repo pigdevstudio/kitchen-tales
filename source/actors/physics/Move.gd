ccec8c64cac65bb282de86a2ce2f80666563212b
Henrique Campos <henriquecampos@pigdev.studio>
2019-10-16 11:22:55 -0300

    Port Jump and Dash
    Simplified Dash behavior, now it is just a Move.gd with higher speed, Move now full Vector2 to apply its speed to the actor's velocity.

--- source/actors/physics/Move.gd
+++ source/actors/physics/Move.gd
@@ -6,12 +6,13 @@ export (Vector2) var direction = Vector2.ZERO setget set_direction
 func execute():
 	if not enabled:
 		return
-	actor.velocity.x = direction.x * speed
+	actor.velocity = direction * speed
 	emit_signal("started")
 
 
 func set_direction(new_direction):
-	direction = new_direction
+	if not new_direction == Vector2.ZERO:
+		direction = new_direction
 
 
 func cancel():
