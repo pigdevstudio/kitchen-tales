41168085fe1158568b5586202727eefcb087fa5a
Henrique Campos <henriquecampos@pigdev.studio>
2019-10-16 16:26:27 -0300

    Add enter and exit methods to States together with an actor property

--- source/actors/physics/State.gd
+++ source/actors/physics/State.gd
@@ -6,8 +6,26 @@ signal exited
 var active = false setget set_active
 var is_moving = false
 
+onready var actor = get_parent().actor setget set_actor
+
 func _ready():
 	connect_commands()
+
+
+func _enter():
+	pass
+
+
+func _exit():
+	pass
+
+
+func set_actor(new_actor):
+	actor = new_actor
+	for command in get_children():
+		if not command.has_method("set_actor"):
+			continue
+		command.set_actor(actor)
 
 
 func connect_commands():
@@ -30,11 +48,15 @@ func _on_command_finished(command):
 
 func set_active(activate):
 	active = activate
+	set_physics_process(active)
+	set_process(active)
 	for command in get_children():
 		command.enabled = active
 	if active:
+		_enter()
 		emit_signal("entered")
 	else:
+		_exit()
 		emit_signal("exited")
 
 
