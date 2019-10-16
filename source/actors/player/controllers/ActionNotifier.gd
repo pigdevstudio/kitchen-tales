c63ae207dd0ec5ae0b4878ce342dbd7b01806de7
Henrique Campos <henriquecampos@pigdev.studio>
2019-10-16 16:31:32 -0300

    Add is_pressed method to ActionNotifier
    This allows an ActionNotifier to be pressed as long as one of the events that build up its action is still pressed, check godotengine/godot/pull/30890 for more

--- source/actors/player/controllers/ActionNotifier.gd
+++ source/actors/player/controllers/ActionNotifier.gd
@@ -21,8 +21,32 @@ func _unhandled_input(event):
 
 
 func handle(event):
-	pressed = event.is_pressed()
+	pressed = is_pressed()
 	if pressed:
 		emit_signal("pressed")
 	else:
 		emit_signal("released")
+
+
+func update_pressed():
+	if pressed:
+		emit_signal("pressed")
+
+
+func is_pressed():
+	var is_pressed = false
+	for event in InputMap.get_action_list(action):
+		if event is InputEventKey:
+			is_pressed = Input.is_key_pressed(event.scancode)
+		elif event is InputEventJoypadButton:
+			is_pressed = Input.is_joy_button_pressed(event.device, event.button_index)
+		elif event is InputEventMouseButton:
+			is_pressed = Input.is_mouse_button_pressed(event.button_index)
+		elif event is InputEventJoypadMotion:
+			is_pressed = Input.get_action_strength(action)
+		else:
+			continue
+		
+		if is_pressed:
+			break
+	return is_pressed
