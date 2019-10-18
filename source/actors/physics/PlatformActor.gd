367efbb5240d79884d2601a4ec7c34411fc1bfab
Henrique Campos <henriquecampos@pigdev.studio>
2019-10-18 19:12:13 -0300

    Added BlackSpoonArena level

--- source/actors/physics/PlatformActor.gd
+++ source/actors/physics/PlatformActor.gd
@@ -7,8 +7,9 @@ var velocity = Vector2(0, 0) setget set_velocity
 var snap_normal = Vector2.DOWN setget set_snap_normal
 
 func _physics_process(delta):
-	velocity = move_and_slide_with_snap(velocity, snap_normal * 50,
-			FLOOR_NORMAL)
+	velocity = move_and_slide_with_snap(velocity, snap_normal * 10,
+			FLOOR_NORMAL, false, 4, deg2rad(46), true)
+
 
 
 func set_snap_normal(new_snap_normal):
