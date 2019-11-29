4390ee0df91905479507f21ba081483fad313225
Henrique Campos <henriquecampos@pigdev.studio>
2019-11-29 19:25:30 -0300

    Close #81

--- source/actors/enemies/onion/GasCloud.gd
+++ source/actors/enemies/onion/GasCloud.gd
@@ -1,36 +1,6 @@
-extends Area2D
+extends Node2D
 
-onready var hitbox = $HitBox
 onready var animator = $AnimationPlayer
-
-func _ready():
-	set_physics_process(false)
-
-
-func _physics_process(delta):
-	pass
-#	slow_down()
-
-
-func slow_down():
-	translate(Vector2.ZERO)
-	for body in get_overlapping_bodies():
-		if not body.is_in_group("player"):
-			continue
-		if not body.has_method("set_velocity"):
-			continue
-		body.velocity.x *= 0.5
-
-
-func _on_area_entered(area):
-	if not animator.current_animation == "vanish":
-		animator.play("damage")
-		set_physics_process(true)
-
-
-func _on_area_exited(area):
-	hitbox.disable()
-	set_physics_process(false)
 
 
 func _on_LifeTime_timeout():
--- source/levels/Spawner.gd
+++ source/levels/Spawner.gd
@@ -6,5 +6,5 @@ export (PackedScene) var spawnling_scene
 func spawn():
 	var spawnling = spawnling_scene.instance()
 	spawnling.global_position = global_position
-	find_parent("Level").add_child(spawnling)
+	find_parent("Level").call_deferred("add_child", spawnling)
 	emit_signal("spawned")
